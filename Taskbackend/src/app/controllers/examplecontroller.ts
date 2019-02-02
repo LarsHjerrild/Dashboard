b7af5723ebe918fbb8e1fa089d17677b81b5610a
larsh <larshjerrild@hotmail.com>
2019-02-02 22:22:57 +0100

    Cleaned code in examplecontroller

--- Taskbackend/src/app/controllers/examplecontroller.ts
+++ Taskbackend/src/app/controllers/examplecontroller.ts
@@ -8,15 +8,22 @@ var logic = require('../../BLL/examplelogic')
 const taskEntry = model<ITaskEntryDocument>("taskEntry");
 
 
-module.exports.getentry = function (req: any, res: any) {
+// module.exports.getentry = function (req: any, res: any) {
+    
+//     logic.ExampleBLL(5, 3).then((data: number) => {
+//         if (!data) {
+            
+//             console.log('Error')
+//             res.status(503)
+//             res.json('fuck')
+//         }
+//         res.status(200);
+//         res.json(data);
+//     })
+// }
 
-    logic.ExampleBLL(5, 3).then((data: number) => {
-        if (!data) {
-
-            console.log('Error')
-            res.status(503)
-            res.json('fuck')
-        }
+module.exports.getModelitem = function (req: any, res: any) {
+    taskEntry.find().then((data) => {
         res.status(200);
         res.json(data);
     })
@@ -39,19 +46,12 @@ module.exports.postModelitem = function (
 
     newEntry.save().then(data => {
         res.status(201);
-        console.log("wee")
         res.json("_id: well");
     });
 
 }
 
 
-module.exports.getModelitem = function (req: any, res: any) {
-    taskEntry.find().then((data) => {
-        res.status(200);
-        res.json(data);
-    })
-}
 
 
 module.exports.updateModelitem = function (
